5394dca0030278f4687b7e053370c93bd13004f3
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-19 10:29:03 +0000

    Updated documentation

--- R/bowl18-min-freq-int.R
+++ R/bowl18-min-freq-int.R
@@ -7,15 +7,19 @@
 #' @param x Chord to analyse.
 #' The default method assumes that the chord is expressed
 #' as a numeric vector of frequencies.
-#' This can be made explicit by using the \code{\link[hrep:pi_chord]{fr_chord}}
-#' function from the \code{hrep} package.
+#' Representations from the hrep package
+#' (e.g. \code{\link[hrep]{pi_chord}()})
+#' can be used to analyse chords expressed as MIDI note numbers.
 #' @return (Numeric scalar)
 #' The minimum distance between the fundamental frequencies of the chord,
 #' in Hz.
 #' @references
 #' \insertRef{Bowling2018}{bowl18}
 #' @rdname bowl18_min_freq_dist
-#' @examples bowl18_min_freq_dist(hrep::fr_chord(c(220, 440, 560)))
+#' @examples
+#' bowl18_min_freq_dist(c(220, 440, 560)) # processed as frequencies
+#' bowl18_min_freq_dist(hrep::fr_chord(c(220, 440, 560))) # same as above
+#' bowl18_min_freq_dist(hrep::pi_chord(c(60, 64, 67))) # MIDI note numbers
 #' @export
 bowl18_min_freq_dist <- function(x) {
   UseMethod("bowl18_min_freq_dist")
